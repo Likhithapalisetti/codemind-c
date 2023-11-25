527edfb7babee7791b54a952e393eb0fb7d9eabc
Likhithapalisetti <149555760+Likhithapalisetti@users.noreply.github.com>
2023-11-25 21:58:49 +0530

    #include<stdio.h> int main() {     int i,j,k;     scanf("%d%d%d",&i,&j,&k);     if(i>50 && i>50)     {         printf("Gryffindor");     }     else if(j>50 && j>50)     {         printf("Slytherin");     }     else if(k>50 && k>50)     {         printf("Hufflepuff");     }     else     {         printf("NOTA");     }
    }

---
+++ Elections_at_Hogwarts.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    scanf("%d%d%d",&i,&j,&k);
+    if(i>50 && i>50)
+    {
+        printf("Gryffindor");
+    }
+    else if(j>50 && j>50)
+    {
+        printf("Slytherin");
+    }
+    else if(k>50 && k>50)
+    {
+        printf("Hufflepuff");
+    }
+    else
+    {
+        printf("NOTA");
+    }
+    
+}
